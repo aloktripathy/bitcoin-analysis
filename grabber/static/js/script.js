1b3c7331929f402759eadf7080f819893b521129
Alok Tripathy <alokrant@gmail.com>
2017-11-05 22:14:44 +0530

    Add auto update

--- grabber/static/js/script.js
+++ grabber/static/js/script.js
@@ -23,31 +23,37 @@ function getChartData(chartParams) {
 }
 
 function drawChart(chartData) {
-    var seriesOptions = [{name: 'buy', data: []}, {name: 'sell', data: []}];
-    var seriesOptions2 = [{name: 'commission', data: []}];
-    var minBuy, maxBuy, minSell, maxSell = 0;
+    var seriesOptions = [
+        {name: 'buy', data: [], color: '#fdecc7', lineColor: '#f8b11c'},
+        {name: 'sell', data: [], color: '#36a2ff'}
+    ];
+    var seriesOptions2 = [{name: 'commission', data: [], color: '#868e96'}];
+    var minBuy, maxBuy, minSell, maxSell, minCommission, maxCommission = 0;
     chartData.ticks.reverse();
     chartData.ticks.forEach(function (val, idx) {
-        if (idx === 0) {
-            minBuy = val.buy;
-            maxBuy = val.buy;
-            minSell = val.sell;
-            maxSell = val.sell;
-        } else {
-            if (val.buy < minBuy) {minBuy = val.buy}
-            if (val.buy > maxBuy) {maxBuy = val.buy}
-            if (val.sell < minSell) {minSell = val.sell}
-            if (val.sell > maxSell) {maxSell = val.sell}
-        }
         var timestamp = new Date(val.time).getTime();
         var commission = (val.buy - val.sell) / val.sell * 100;
         seriesOptions[0].data.push([timestamp, val.buy]);
         seriesOptions[1].data.push([timestamp, val.sell]);
         seriesOptions2[0].data.push([timestamp, parseFloat(commission.toFixed(2))]);
+        if (idx === 0) {
+            minBuy = val.buy;
+            maxBuy = val.buy;
+            minSell = val.sell;
+            maxSell = val.sell;
+            minCommission = commission;
+            maxCommission = commission;
+        } else {
+            if (val.buy < minBuy) {minBuy = val.buy}
+            if (val.buy > maxBuy) {maxBuy = val.buy}
+            if (val.sell < minSell) {minSell = val.sell}
+            if (val.sell > maxSell) {maxSell = val.sell}
+            if (commission < minCommission) {minCommission = commission}
+            if (commission > maxCommission) {maxCommission = commission}
+        }
     });
     var chartFloor = Math.min(minBuy, minSell);
     var chartCeiling = Math.max(maxBuy, maxSell);
-    console.log(chartCeiling, chartFloor);
     var buyPercentageChange = 0;
     var sellPercentageChange = 0;
     if (chartData.ticks.length > 1) {
@@ -62,7 +68,7 @@ function drawChart(chartData) {
     }
     Highcharts.setOptions({global: {useUTC: false}});
     Highcharts.chart('chart-container', {
-        chart: {zoomType: 'x'},
+        chart: {zoomType: 'x', 'type': 'area', height: 450},
         subtitle: {text: chartData.crypto_currency.toUpperCase() + ' to ' + chartData.exchange_currency.toUpperCase() + ' exchange rate over time'},
         title: {
             text: 'BUY ' + buyPercentageChange + '% | SELL ' + sellPercentageChange + '%',
@@ -75,14 +81,27 @@ function drawChart(chartData) {
             title: {text: 'Exchange rate'}
         },
         legend: {enabled: false},
-        plotOptions: {},
+        plotOptions: {
+            area: {
+                marker: {
+                    enabled: false,
+                    symbol: 'circle',
+                    radius: 2,
+                    states: {hover: {enabled: true}}
+                }
+            }
+        },
         series: seriesOptions
     });
     Highcharts.chart('commission-container', {
-        chart: {zoomType: 'x'},
+        chart: {zoomType: 'x', type: 'area'},
         title: {text: chartData.crypto_currency.toUpperCase() + ' to ' + chartData.exchange_currency.toUpperCase() + ' exchange commission rate over time'},
         xAxis: {type: 'datetime'},
-        yAxis: {title: {text: 'Commission rate'}},
+        yAxis: {
+            title: {text: 'Commission rate'},
+            gridLineColor: 'transparent',
+            tickPositions: [parseFloat(minCommission.toFixed(2)), parseFloat(maxCommission.toFixed(2))]
+        },
         legend: {enabled: false},
         series: seriesOptions2
     });
@@ -105,4 +124,7 @@ window.onload = function () {
         plot()
     });
     plot();
+    setInterval(function () {
+        plot();
+    }, 2000);
 };
