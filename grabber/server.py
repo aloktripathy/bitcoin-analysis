5ebd176242c3e1cab8b1420c83a9d05aff15d1d3
Alok Tripathy <alokrant@gmail.com>
2017-11-05 14:55:45 +0530

    Add bottle server

---
+++ grabber/server.py
@@ -0,0 +1,12 @@
+from bottle import route, run, template
+
+
+HOST = 'localhost'
+PORT = 8080
+
+
+@route('/hello/<name>')
+def index(name):
+    return template('<b>Hello {{name}}</b>!', name=name)
+
+run(host=HOST, port=PORT)
