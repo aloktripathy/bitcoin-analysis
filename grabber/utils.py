b8dd3200c852e9f35dbab5a271b67f22beec6a47
Alok Tripathy <alokrant@gmail.com>
2017-11-05 17:24:20 +0530

    Add ticker api

--- grabber/utils.py
+++ grabber/utils.py
@@ -3,9 +3,14 @@ import pytz
 import time
 
 
-def get_utc_now():
+def utc_now():
     return pytz.utc.localize(datetime.datetime.utcnow())
 
 
-def get_epoch_timestamp():
+def get_now_timestamp():
     return time.time()
+
+
+def get_utc_time_from_timestamp(ts):
+    epoch_time = datetime.datetime(1970, 1, 1, 0, 0, 0, 0, tzinfo=pytz.utc)
+    return epoch_time + datetime.timedelta(seconds=ts)
